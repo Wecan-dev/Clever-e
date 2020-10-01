5572d93a0ef86d5416a94976b395173b24ff3411
navasg <genenavas97@gmail.com>
2020-10-01 09:31:19 -0400

    issues qa

--- wp-content/themes/clever/woocommerce/myaccount/lost-password-confirmation.php
+++ wp-content/themes/clever/woocommerce/myaccount/lost-password-confirmation.php
@@ -25,3 +25,11 @@ wc_print_notice( esc_html__( 'Password reset email has been sent.', 'woocommerce
 <p><?php echo esc_html( apply_filters( 'woocommerce_lost_password_confirmation_message', esc_html__( 'A password reset email has been sent to the email address on file for your account, but may take several minutes to show up in your inbox. Please wait at least 10 minutes before attempting another reset.', 'woocommerce' ) ) ); ?></p>
 
 <?php do_action( 'woocommerce_after_lost_password_confirmation_message' ); ?>
+
+<style>
+
+p {
+  font-size: 16px;
+  padding: 10px;
+}
+</style>
