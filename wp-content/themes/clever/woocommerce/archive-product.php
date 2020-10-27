716d267098cfd7c80c7bef213800ac0f9d8623b7
Misledy Alastre <mislejose@gmail.com>
2020-10-26 16:11:23 -1200

    Add banner modal on single

--- wp-content/themes/clever/woocommerce/archive-product.php
+++ wp-content/themes/clever/woocommerce/archive-product.php
@@ -244,6 +244,73 @@ else { ?>
 					<?php endforeach; ?> 		
 				</div>
 				<?php // get_template_part('sections/products/medidas'); ?>
+<!-- Medidas -->
+<div class="categories-sidebar__size">
+	<img src="<?php echo get_template_directory_uri();?>/assets/img/categorie/size.png">
+	<div class="categories-sidebar__text">
+		<p><?php if(lang() == 'es'){echo "Tabla de medidas de <br> acuerdo con tu cuerpo";}else{echo "Measurement table <br> according to your body";} ?></p>
+		<a data-target="#exampleModal" data-toggle="modal"><?php if(lang() == 'es'){echo "VER MÁS";}else{echo "SEE MORE";} ?></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade modal-size" id="exampleModal" role="dialog" tabindex="-1">
+		<div class="modal-dialog" role="document">
+        <?php if (terms_silueta( terms_id( $_GET["cat"] ) ) == "pa_silueta" OR terms_silueta( terms_id( $_GET["cat"] ) ) == "pa_silhouette") { ?>
+            <div class="modal-content" style="background-image: url(<?php echo termmeta_value_img( 'image_banner_categories', terms_id( $_GET["cat"] ) ); ?>);">
+        <?php }else { ?>
+	        <div class="modal-content">
+	    <?php } ?>			
+				<button aria-label="Close" class="close" data-dismiss="modal" type="button">
+					<span aria-hidden="true">×</span>
+				</button>
+				<div class="modal-body">
+					<h2 class="modal-size__title">
+						<?php if(lang() == 'es'){echo "Tabla de <br><span>Medidas</span>";}else{echo "measurement <br><span>table</span>";} ?>
+					</h2>
+					<div class="modal-size-tab">
+						<img src="<?php echo get_template_directory_uri();?>/assets/img/categorie/lines.png">
+						<p class="modal-size__header">
+							<?php if(lang() == 'es'){echo "Prendas inferiores";}else{echo "Lower garments";} ?>
+						</p>
+						<div class="modal-size__ref">
+							<div class="modal-size__item">
+								<p><?php if(lang() == 'es'){echo "Categorías";}else{echo "Categories";} ?></p>
+							</div>
+							<div class="modal-size__item">
+								<p>S</p>
+							</div>
+							<div class="modal-size__item">
+								<p>M</p>
+							</div>
+							<div class="modal-size__item">
+								<p>L</p>
+							</div>
+							<div class="modal-size__item">
+								<p>XL</p>
+							</div>
+						</div>
+						<div class="modal-size__row">
+							<div class="modal-size__row--item">
+								<p><?php if(lang() == 'es'){echo "Cintura";}else{echo "Waist";} ?></p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>28-30</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>30-32</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>32-36</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>34-36</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+				
 			</div>
 		</div>
 		<div class="categories-product">
@@ -435,75 +502,6 @@ else { ?>
 				</div>
 			</div>
 		</section>
-
-
-<!-- Medidas -->
-<div class="categories-sidebar__size">
-	<img src="<?php echo get_template_directory_uri();?>/assets/img/categorie/size.png">
-	<div class="categories-sidebar__text">
-		<p><?php if(lang() == 'es'){echo "Tabla de medidas de <br> acuerdo con tu cuerpo";}else{echo "Measurement table <br> according to your body";} ?></p>
-		<a data-target="#exampleModal" data-toggle="modal"><?php if(lang() == 'es'){echo "VER MÁS";}else{echo "SEE MORE";} ?></a>
-	</div>
-	<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade modal-size" id="exampleModal" role="dialog" tabindex="-1">
-		<div class="modal-dialog" role="document">
-        <?php if (terms_silueta( terms_id( $_GET["cat"] ) ) == "pa_silueta" OR terms_silueta( terms_id( $_GET["cat"] ) ) == "pa_silhouette") { ?>
-            <div class="modal-content" style="background-image: url(<?php echo termmeta_value_img( 'image_banner_categories', terms_id( $_GET["cat"] ) ); ?>);">
-        <?php }else { ?>
-	        <div class="modal-content">
-	    <?php } ?>			
-				<button aria-label="Close" class="close" data-dismiss="modal" type="button">
-					<span aria-hidden="true">×</span>
-				</button>
-				<div class="modal-body">
-					<h2 class="modal-size__title">
-						<?php if(lang() == 'es'){echo "Tabla de <br><span>Medidas</span>";}else{echo "measurement <br><span>table</span>";} ?>
-					</h2>
-					<div class="modal-size-tab">
-						<img src="<?php echo get_template_directory_uri();?>/assets/img/categorie/lines.png">
-						<p class="modal-size__header">
-							<?php if(lang() == 'es'){echo "Prendas inferiores";}else{echo "Lower garments";} ?>
-						</p>
-						<div class="modal-size__ref">
-							<div class="modal-size__item">
-								<p><?php if(lang() == 'es'){echo "Categorías";}else{echo "Categories";} ?></p>
-							</div>
-							<div class="modal-size__item">
-								<p>S</p>
-							</div>
-							<div class="modal-size__item">
-								<p>M</p>
-							</div>
-							<div class="modal-size__item">
-								<p>L</p>
-							</div>
-							<div class="modal-size__item">
-								<p>XL</p>
-							</div>
-						</div>
-						<div class="modal-size__row">
-							<div class="modal-size__row--item">
-								<p><?php if(lang() == 'es'){echo "Cintura";}else{echo "Waist";} ?></p>
-							</div>
-							<div class="modal-size__row--item">
-								<p>28-30</p>
-							</div>
-							<div class="modal-size__row--item">
-								<p>30-32</p>
-							</div>
-							<div class="modal-size__row--item">
-								<p>32-36</p>
-							</div>
-							<div class="modal-size__row--item">
-								<p>34-36</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
 
 		<style type="text/css">
 			a.page-numbers {
--- wp-content/themes/clever/woocommerce/content-single-product.php
+++ wp-content/themes/clever/woocommerce/content-single-product.php
@@ -34,7 +34,8 @@ if ( post_password_required() ) {
 <section class="product-details padding-left-right">
 
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
-
+<?php $product_categories_en = wp_get_post_terms( get_the_ID(), 'pa_silhouette' )[0]->term_id; ?>
+<?php $product_categories_es = wp_get_post_terms( get_the_ID(), 'pa_silueta' )[0]->term_id; ?>
 	<?php
 	/**
 	 * Hook: woocommerce_before_single_product_summary.
@@ -72,7 +73,79 @@ if ( post_password_required() ) {
 		 */
 		do_action( 'woocommerce_single_product_summary' );
 		?>
-	  <?php  get_template_part('sections/products/medidas'); ?>
+	  <?php // get_template_part('sections/products/medidas'); ?>
+<!-- Medidas -->
+<div class="categories-sidebar__size">
+	<img src="<?php echo get_template_directory_uri();?>/assets/img/categorie/size.png">
+	<div class="categories-sidebar__text">
+		<p><?php if(lang() == 'es'){echo "Tabla de medidas de <br> acuerdo con tu cuerpo";}else{echo "Measurement table <br> according to your body";} ?></p>
+		<a data-target="#exampleModal" data-toggle="modal"><?php if(lang() == 'es'){echo "VER MÁS";}else{echo "SEE MORE";} ?></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade modal-size" id="exampleModal" role="dialog" tabindex="-1">
+		<div class="modal-dialog" role="document">
+        <?php if($product_categories_en != NULL OR $product_categories_es != NULL){ ?>
+            <?php if(lang() == 'es'){ ?>
+                <div class="modal-content" style="background-image: url(<?php echo termmeta_value_img( 'image_banner_categories', $product_categories_es ); ?>);">
+            <?php } ?> 
+            <?php if(lang() == 'en'){ ?>
+                <div class="modal-content" style="background-image: url(<?php echo termmeta_value_img( 'image_banner_categories', $product_categories_en ); ?>);">
+            <?php } ?>               
+        <?php }else { ?>
+	        <div class="modal-content">
+	    <?php } ?>			
+				<button aria-label="Close" class="close" data-dismiss="modal" type="button">
+					<span aria-hidden="true">×</span>
+				</button>
+				<div class="modal-body">
+					<h2 class="modal-size__title">
+						<?php if(lang() == 'es'){echo "Tabla de <br><span>Medidas</span>";}else{echo "measurement <br><span>table</span>";} ?>
+					</h2>
+					<div class="modal-size-tab">
+						<img src="<?php echo get_template_directory_uri();?>/assets/img/categorie/lines.png">
+						<p class="modal-size__header">
+							<?php if(lang() == 'es'){echo "Prendas inferiores";}else{echo "Lower garments";} ?>
+						</p>
+						<div class="modal-size__ref">
+							<div class="modal-size__item">
+								<p><?php if(lang() == 'es'){echo "Categorías";}else{echo "Categories";} ?></p>
+							</div>
+							<div class="modal-size__item">
+								<p>S</p>
+							</div>
+							<div class="modal-size__item">
+								<p>M</p>
+							</div>
+							<div class="modal-size__item">
+								<p>L</p>
+							</div>
+							<div class="modal-size__item">
+								<p>XL</p>
+							</div>
+						</div>
+						<div class="modal-size__row">
+							<div class="modal-size__row--item">
+								<p><?php if(lang() == 'es'){echo "Cintura";}else{echo "Waist";} ?></p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>28-30</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>30-32</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>32-36</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>34-36</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 	<!-- <?php//
 	/**
 	 * Hook: woocommerce_after_single_product_summary.
@@ -86,6 +159,64 @@ if ( post_password_required() ) {
 </div>
 <?php //do_action( 'woocommerce_after_single_product' ); ?>
 </section>
+
+
+
+	<div aria-hidden="true" aria-labelledby="exampleModalLabel" class="modal fade modal-size" id="exampleModal" role="dialog" tabindex="-1">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<button aria-label="Close" class="close" data-dismiss="modal" type="button">
+					<span aria-hidden="true">×</span>
+				</button>
+				<div class="modal-body">
+					<h2 class="modal-size__title">
+						<?php if(lang() == 'es'){echo "Tabla de <br><span>Medidas</span>";}else{echo "measurement <br><span>table</span>";} ?>
+					</h2>
+					<div class="modal-size-tab">
+						<img src="<?php echo get_template_directory_uri();?>/assets/img/categorie/lines.png">
+						<p class="modal-size__header">
+							<?php if(lang() == 'es'){echo "Prendas inferiores";}else{echo "Lower garments";} ?>
+						</p>
+						<div class="modal-size__ref">
+							<div class="modal-size__item">
+								<p><?php if(lang() == 'es'){echo "Categorías";}else{echo "Categories";} ?></p>
+							</div>
+							<div class="modal-size__item">
+								<p>S</p>
+							</div>
+							<div class="modal-size__item">
+								<p>M</p>
+							</div>
+							<div class="modal-size__item">
+								<p>L</p>
+							</div>
+							<div class="modal-size__item">
+								<p>XL</p>
+							</div>
+						</div>
+						<div class="modal-size__row">
+							<div class="modal-size__row--item">
+								<p><?php if(lang() == 'es'){echo "Cintura";}else{echo "Waist";} ?></p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>28-30</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>30-32</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>32-36</p>
+							</div>
+							<div class="modal-size__row--item">
+								<p>34-36</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 
 <script type="text/javascript">
   $('.woocommerce-breadcrumb').appendTo('.nav-content-product');
