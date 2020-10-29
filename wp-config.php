9c546f08c4facc9ef3036383d3c491d42630df4f
Misledy <misledy1@hotmail.com>
2020-10-29 13:55:43 -0400

    Merge

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'db_clever' );
 
 /** MySQL database username */
-define( 'DB_USER', 'admin' );
+define( 'DB_USER', 'adminwecan' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '1234' );
+define( 'DB_PASSWORD', '_*8gTYWqM9FHU' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
