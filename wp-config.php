d7f2ac37e4fbc78238bf19da0e5041f81f25ad7b
Misledy <misledy1@hotmail.com>
2020-10-30 10:12:02 -0400

    Add dropdown lang

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'db_clever' );
 
 /** MySQL database username */
-define( 'DB_USER', 'adminwecan' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '_*8gTYWqM9FHU' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
