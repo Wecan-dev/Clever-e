b88b448d55b819fa32aa26f7b1d4f12ee1abc4be
Misledy <misledy1@hotmail.com>
2020-11-05 09:24:09 -0400

    Corection url lang

--- wp-content/themes/clever/header.php
+++ wp-content/themes/clever/header.php
@@ -25,7 +25,7 @@
           <?php echo get_theme_mod('prenavbar'.$i.'_line2_'.lang().''); ?>
         </p>
         <a class="main-general__button" href="<?php echo get_theme_mod('prenavbar'.$i.'_urlbutton_'.lang().''); ?>"><?php echo get_theme_mod('prenavbar'.$i.'_button_'.lang().''); ?><img class="icon-tarjeta" src="<?php echo get_template_directory_uri();?>/assets/img/next.png"></a>
-      </div>t
+      </div>
     </div>
    <?php }} ?>
   </div>
@@ -109,10 +109,11 @@
               </ul>  
                 <ul class="navbar-nav mr-autos">
                   <li class="nav-item dropdown drop-money">
-                    <a aria-expanded="false" aria-haspopup="true" class="nav-world nav-link dropdown-toggle" data-toggle="dropdown" href="catalogo.html" role="button"><?php if (lang() == 'es'){ echo "Idioma";}else{ echo "Language";}?></a>
+                    <a aria-expanded="false" aria-haspopup="true" class="nav-world nav-link dropdown-toggle" data-toggle="dropdown" href="catalogo.html" role="button"><?php $url = explode('?', $_SERVER['REQUEST_URI'], 2);
+                      if (lang() == 'es'){ echo "Idioma"; }else{ echo "Language"; $url = explode('en', $_SERVER['REQUEST_URI'], 2);}  ?></a>
                     <div class="dropdown-menu">
-                      <a class="dropdown-item woocs_flag_view_item" title="English" lang="en-US" hreflang="en-US" href="http://localhost/Clever-e/en/">English</a>
-                      <a class="dropdown-item woocs_flag_view_item" title="Español"lang="es-CO" hreflang="es-CO" href="http://localhost/Clever-e/">Español</a>
+                      <a class="dropdown-item woocs_flag_view_item" title="English" lang="en-US" hreflang="en-US" href="<?php echo $url[0]; ?>en/">English</a>
+                      <a class="dropdown-item woocs_flag_view_item" title="Español"lang="es-CO" hreflang="es-CO" href="<?php echo $url[0]; ?>">Español</a>
                     </div>
                   </li>
                 </ul>                          
@@ -158,10 +159,11 @@
 
                 <ul class="navbar-nav mr-autos">
                   <li class="nav-item dropdown drop-money">
-                    <a aria-expanded="false" aria-haspopup="true" class="nav-world nav-link dropdown-toggle" data-toggle="dropdown" href="catalogo.html" role="button"><?php if (lang() == 'es'){ echo "Idioma";}else{ echo "Language";}?></a>
+                    <a aria-expanded="false" aria-haspopup="true" class="nav-world nav-link dropdown-toggle" data-toggle="dropdown" href="catalogo.html" role="button"><?php $url = explode('?', $_SERVER['REQUEST_URI'], 2);
+                      if (lang() == 'es'){ echo "Idioma"; }else{ echo "Language"; $url = explode('en', $_SERVER['REQUEST_URI'], 2);}  ?></a>
                     <div class="dropdown-menu">
-                      <a class="dropdown-item woocs_flag_view_item" lang="en-US" hreflang="en-US" href="http://localhost/Clever-e/en/">English</a>
-                      <a class="dropdown-item woocs_flag_view_item" data-currency="COP" title="COP, $ Peso Colombiano"lang="es-CO" hreflang="es-CO" href="http://localhost/Clever-e/">Español</a>
+                      <a class="dropdown-item woocs_flag_view_item" title="English" lang="en-US" hreflang="en-US" href="<?php echo $url[0]; ?>en/">English</a>
+                      <a class="dropdown-item woocs_flag_view_item" title="Español"lang="es-CO" hreflang="es-CO" href="<?php echo $url[0]; ?>">Español</a>
                     </div>
                   </li>
                 </ul> 
@@ -283,13 +285,14 @@
 
                 <ul class="navbar-nav mr-autos">
                   <li class="nav-item dropdown drop-money">
-                    <a aria-expanded="false" aria-haspopup="true" class="nav-world nav-link dropdown-toggle" data-toggle="dropdown" href="catalogo.html" role="button"><?php if (lang() == 'es'){ echo "Idioma";}else{ echo "Language";}?></a>
+                    <a aria-expanded="false" aria-haspopup="true" class="nav-world nav-link dropdown-toggle" data-toggle="dropdown" href="catalogo.html" role="button"><?php $url = explode('?', $_SERVER['REQUEST_URI'], 2);
+                      if (lang() == 'es'){ echo "Idioma"; }else{ echo "Language"; $url = explode('en', $_SERVER['REQUEST_URI'], 2);}  ?></a>
                     <div class="dropdown-menu">
-                      <a class="dropdown-item woocs_flag_view_item" title="English" lang="en-US" hreflang="en-US" href="http://localhost/Clever-e/en/">English</a>
-                      <a class="dropdown-item woocs_flag_view_item" title="Español"lang="es-CO" hreflang="es-CO" href="http://localhost/Clever-e/">Español</a>
+                      <a class="dropdown-item woocs_flag_view_item" title="English" lang="en-US" hreflang="en-US" href="<?php echo $url[0]; ?>en/">English</a>
+                      <a class="dropdown-item woocs_flag_view_item" title="Español"lang="es-CO" hreflang="es-CO" href="<?php echo $url[0]; ?>">Español</a>
                     </div>
                   </li>
-                </ul>              
+                </ul>               
               <!--<?php if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
               <aside class="in-header widget-area right" role="complementary">
                 <?php dynamic_sidebar( 'sidebar-1' ); ?>
